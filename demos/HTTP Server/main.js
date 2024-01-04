835f95a67e4cec334d8afec7bb26b87274fc3953
AkshayWarrier <aksmen121@gmail.com>
2024-01-04 22:19:50 +0530

    Launch uri on click

--- demos/HTTP Server/main.js
+++ demos/HTTP Server/main.js
@@ -1,4 +1,8 @@
+import Gio from "gi://Gio";
+import Gtk from "gi://Gtk";
 import Soup from "gi://Soup";
+
+Gio._promisify(Gtk.UriLauncher.prototype, "launch", "launch_finish");
 
 function handler(_server, msg, _path, _query) {
   msg.set_status(200, null);
@@ -41,6 +45,7 @@ function helloHandler(_server, msg, path, query) {
 let button_server = workbench.builder.get_object("button_server");
 
 let server = new Soup.Server();
+let uri_launcher = new Gtk.UriLauncher({ uri: "http://localhost:1080" });
 server.add_handler("/", handler);
 server.add_handler("/hello", helloHandler);
 
@@ -48,6 +53,7 @@ button_server.connect("clicked", () => {
   if (button_server.active) {
     server.listen_local(1080, Soup.ServerListenOptions.IPV4_ONLY);
     button_server.label = "Stop Server";
+    uri_launcher.launch(workbench.window, null).catch(console.error);
   } else {
     server.disconnect();
     button_server.label = "Start Server";
