d78955b570b915add1c8449cd617aff1e10ca976
Sonny Piers <sonny@fastmail.net>
2024-01-07 16:42:26 +0100

    tweak

--- demos/HTTP Server/main.js
+++ demos/HTTP Server/main.js
@@ -1,8 +1,8 @@
-import Gio from "gi://Gio";
-import Gtk from "gi://Gtk";
 import Soup from "gi://Soup";
 
-Gio._promisify(Gtk.UriLauncher.prototype, "launch", "launch_finish");
+const button_server = workbench.builder.get_object("button_server");
+const linkbutton = workbench.builder.get_object("linkbutton");
+const label_greetings = workbench.builder.get_object("label_greetings");
 
 function handler(_server, msg, _path, _query) {
   msg.set_status(200, null);
@@ -10,52 +10,66 @@ function handler(_server, msg, _path, _query) {
     .get_response_headers()
     .set_content_type("text/html", { charset: "UTF-8" });
   msg.get_response_body().append(`
-        <html>
-        <body>
-            Greetings, visitor from ${msg.get_remote_host()}<br>
-            What is your name?
-            <form action="/hello">
-                <input name="myname">
-            </form>
-        </body>
-        </html>
-    `);
+    <html>
+    <body>
+      <form action="/hello">
+        <label for="name">What is your name?</label>
+        <input name="name">
+        <input type="submit" value="Submit">
+      </form>
+    </body>
+    </html>
+  `);
 }
 
-function helloHandler(_server, msg, path, query) {
+function helloHandler(_server, msg, _path, query) {
   if (!query) {
     msg.set_redirect(302, "/");
     return;
   }
+
+  const user_agent = msg.get_request_headers().get_one("User-Agent");
+
+  label_greetings.label = `Hello ${query.name}, your browser is\n${user_agent}`;
 
   msg.set_status(200, null);
   msg
     .get_response_headers()
     .set_content_type("text/html", { charset: "UTF-8" });
   msg.get_response_body().append(`
-        <html>
-        <body>
-            Hello, ${query.myname}! ☺<br>
-            <a href="/">Go back</a>
-        </body>
-        </html>
-    `);
+    <html>
+    <body>
+      Thank you, please go back to Workbench.
+    </body>
+    </html>
+  `);
 }
 
-let button_server = workbench.builder.get_object("button_server");
+const server = new Soup.Server();
 
-let server = new Soup.Server();
-let uri_launcher = new Gtk.UriLauncher({ uri: "http://localhost:1080" });
 server.add_handler("/", handler);
 server.add_handler("/hello", helloHandler);
 
 button_server.connect("clicked", () => {
   if (button_server.active) {
-    server.listen_local(1080, Soup.ServerListenOptions.IPV4_ONLY);
-    button_server.label = "Stop Server";
-    uri_launcher.launch(workbench.window, null).catch(console.error);
+    startServer();
   } else {
-    server.disconnect();
-    button_server.label = "Start Server";
+    stopServer();
   }
 });
+
+let port = 0;
+
+function startServer() {
+  server.listen_local(port, null);
+  port = server.get_uris()[0].get_port();
+  linkbutton.uri = `http://localhost:${port}`;
+  button_server.label = "Stop Server";
+}
+
+function stopServer() {
+  server.disconnect();
+  linkbutton.uri = "";
+  button_server.label = "Start Server";
+}
+
