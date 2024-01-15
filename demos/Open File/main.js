4c29b8cfede08649c29149e7802b253b297abc9c
Sahil Shadwal <sahilshadwal@gmail.com>
2024-01-15 23:15:30 +0530

    Added filter for various file types .

--- demos/Open File/main.js
+++ demos/Open File/main.js
@@ -23,13 +23,48 @@ async function openFile() {
 }
 
 async function openMultipleFiles() {
-  const dialog_for_multiple_files = new Gtk.FileDialog();
-  const files = await dialog_for_multiple_files.open_multiple(
-    workbench.window,
-    null,
-  );
-  const selected_items_count = files.get_n_items();
-  console.log(`No of selected files: ${selected_items_count}`);
+  const filters = new Gio.ListStore();
+
+  const textFilter = Gtk.FileFilter.new();
+  textFilter.set_name("Text Files");
+  textFilter.add_mime_type("text/*");
+  filters.append(textFilter);
+
+  const imageFilter = Gtk.FileFilter.new();
+  imageFilter.set_name("Image Files");
+  imageFilter.add_mime_type("image/*");
+  filters.append(imageFilter);
+
+  const audioFilter = Gtk.FileFilter.new();
+  audioFilter.set_name("Audio Files");
+  audioFilter.add_mime_type("audio/*");
+  filters.append(audioFilter);
+
+  const videoFilter = Gtk.FileFilter.new();
+  videoFilter.set_name("Video Files");
+  videoFilter.add_mime_type("video/*");
+  filters.append(videoFilter);
+
+  const pdfFilter = Gtk.FileFilter.new();
+  pdfFilter.set_name("PDF Documents");
+  pdfFilter.add_mime_type("application/pdf");
+  filters.append(pdfFilter);
+
+  const wordFilter = Gtk.FileFilter.new();
+  wordFilter.set_name("Word Documents");
+  wordFilter.add_mime_type("application/msword");
+  filters.append(wordFilter);
+
+  const zipFilter = Gtk.FileFilter.new();
+  zipFilter.set_name("ZIP Archives");
+  zipFilter.add_mime_type("application/zip");
+  filters.append(zipFilter);
+
+  const fileDialog = new Gtk.FileDialog({ filters });
+
+  const files = await fileDialog.open_multiple(workbench.window, null);
+  const selectedItemsCount = files.get_n_items();
+  console.log(`No of selected files: ${selectedItemsCount}`);
 }
 
 button_single.connect("clicked", () => {
