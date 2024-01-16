859930168fa54c6197a7a8959c9507a476405b26
Sahil Shadwal <sahilshadwal@gmail.com>
2024-01-17 00:19:18 +0530

    Added logic for opening image using filter .

--- demos/Open File/main.js
+++ demos/Open File/main.js
@@ -10,6 +10,9 @@ Gio._promisify(
 
 const button_single = workbench.builder.get_object("button_single");
 const button_multiple = workbench.builder.get_object("button_multiple");
+const button_multiple_images = workbench.builder.get_object(
+  "button_multiple_images",
+);
 
 async function openFile() {
   const dialog_for_file = new Gtk.FileDialog();
@@ -23,42 +26,22 @@ async function openFile() {
 }
 
 async function openMultipleFiles() {
-  const filters = new Gio.ListStore();
+  const dialog_for_multiple_files = new Gtk.FileDialog();
+  const files = await dialog_for_multiple_files.open_multiple(
+    workbench.window,
+    null,
+  );
+  const selected_items_count = files.get_n_items();
+  console.log(`No of selected files: ${selected_items_count}`);
+}
 
-  const textFilter = Gtk.FileFilter.new();
-  textFilter.set_name("Text Files");
-  textFilter.add_mime_type("text/*");
-  filters.append(textFilter);
+async function openMultipleImageFiles() {
+  const filters = new Gio.ListStore();
 
   const imageFilter = Gtk.FileFilter.new();
   imageFilter.set_name("Image Files");
   imageFilter.add_mime_type("image/*");
   filters.append(imageFilter);
-
-  const audioFilter = Gtk.FileFilter.new();
-  audioFilter.set_name("Audio Files");
-  audioFilter.add_mime_type("audio/*");
-  filters.append(audioFilter);
-
-  const videoFilter = Gtk.FileFilter.new();
-  videoFilter.set_name("Video Files");
-  videoFilter.add_mime_type("video/*");
-  filters.append(videoFilter);
-
-  const pdfFilter = Gtk.FileFilter.new();
-  pdfFilter.set_name("PDF Documents");
-  pdfFilter.add_mime_type("application/pdf");
-  filters.append(pdfFilter);
-
-  const wordFilter = Gtk.FileFilter.new();
-  wordFilter.set_name("Word Documents");
-  wordFilter.add_mime_type("application/msword");
-  filters.append(wordFilter);
-
-  const zipFilter = Gtk.FileFilter.new();
-  zipFilter.set_name("ZIP Archives");
-  zipFilter.add_mime_type("application/zip");
-  filters.append(zipFilter);
 
   const fileDialog = new Gtk.FileDialog({ filters });
 
@@ -73,4 +56,8 @@ button_single.connect("clicked", () => {
 
 button_multiple.connect("clicked", () => {
   openMultipleFiles().catch(console.error);
+});
+
+button_multiple_images.connect("clicked", () => {
+  openMultipleImageFiles().catch(console.error);
 });
