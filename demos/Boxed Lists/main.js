1ee50d215e11bd095f46a4382c5be589f7c92562
Sahil Shadwal <sahilshadwal@gmail.com>
2024-02-07 15:07:55 +0530

    added js file for enabling search

---
+++ demos/Boxed Lists/main.js
@@ -0,0 +1,18 @@
+import GObject from "gi://GObject";
+import Gtk from "gi://Gtk";
+
+const drop_down = workbench.builder.get_object("drop_down");
+
+drop_down.connect("notify::selected-item", () => {
+  const selected_item = drop_down.selected_item.get_string();
+  console.log(selected_item);
+});
+
+const expression = new Gtk.ClosureExpression(
+  GObject.TYPE_STRING,
+  (obj) => obj.string,
+  null,
+);
+
+drop_down.expression = expression;
+
