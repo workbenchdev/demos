a88e40aa925308e89bf500c8f27193e88763ece2
urffsamhunt <iwriterofficial511@gmail.com>
2024-04-01 01:08:43 +0530

    Fix Rust implementation of LinkButton demo

--- src/Link Button/code.rs
+++ src/Link Button/code.rs
@@ -1,15 +1,15 @@
 use crate::workbench;
 use gtk::prelude::*;
-use gtk::traits::ButtonExt;
 
 pub fn main() {
     let linkbutton: gtk::LinkButton = workbench::builder().object("linkbutton").unwrap();
 
-    linkbutton.connect_clicked(move |_| {
-        println!("Link has been visited");
+    linkbutton.connect_activate_link(|button| {
+        println!("About to activate {}", button.uri());
+        return false.into();
     });
 
-    linkbutton.connect_notify(Some("visited"), |button, _| {
-        println!("About to activate {}", button.uri());
+    linkbutton.connect_notify(Some("visited"), |_, _| {
+        println!("The link has been visited");
     });
 }
