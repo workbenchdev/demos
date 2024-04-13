f2f96fd5b893c29d17ba78dc4ebdbc0012541935
Sameer Patel <49314145+urffsamhunt@users.noreply.github.com>
2024-04-14 00:06:22 +0530

    Ported Link Button Demo to Vala (#131)

--- src/Link Button/code.rs
+++ src/Link Button/code.rs
@@ -1,15 +1,17 @@
 use crate::workbench;
+use glib::signal::Propagation;
+use gtk::glib;
 use gtk::prelude::*;
-use gtk::traits::ButtonExt;
 
 pub fn main() {
     let linkbutton: gtk::LinkButton = workbench::builder().object("linkbutton").unwrap();
 
-    linkbutton.connect_clicked(move |_| {
-        println!("Link has been visited");
+    linkbutton.connect_activate_link(|button| {
+        println!("About to activate {}", button.uri());
+        Propagation::Proceed
     });
 
-    linkbutton.connect_notify(Some("visited"), |button, _| {
-        println!("About to activate {}", button.uri());
+    linkbutton.connect_notify(Some("visited"), |_, _| {
+        println!("The link has been visited");
     });
 }
