94b6cedc9b7c34354b922e7d14aa1f0e25a24df3
Ondrej Kolin <okolin@gk-software.com>
2024-07-30 14:41:14 +0200

    Audio demo working

---
+++ src/Audio/code.rs
@@ -0,0 +1,38 @@
+use crate::workbench;
+use gtk::glib;
+use gtk::prelude::*;
+use std::collections::HashMap;
+
+pub fn main() {
+    //Otherwise the MediaFile crate crashes
+    gtk::init().unwrap();
+
+    let controls: gtk::MediaControls = workbench::builder().object("controls").unwrap();
+
+    let audio_files = HashMap::from([
+        ("sound", "./Dog.ogg"),
+        ("music", "./Chopin-nocturne-op-9-no-2.ogg"),
+    ]);
+
+    for (button_name, file_name) in audio_files {
+        let button: gtk::Button = workbench::builder()
+            .object(format!("button_{}", button_name))
+            .unwrap();
+
+        button.connect_clicked(glib::clone!(
+          @weak controls => move |_| {
+            if let Some(media_stream) = controls.media_stream() {
+                media_stream.set_playing(false);
+            }
+
+            controls.set_media_stream(Some(&gtk::MediaFile::for_file(
+              &gtk::gio::File::for_uri(&workbench::resolve(file_name))
+            )));
+            // Media stream can be missing
+            if let Some(media_stream) = controls.media_stream() {
+                media_stream.set_playing(true);
+            }
+          }
+        ));
+    }
+}
