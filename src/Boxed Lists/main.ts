cf516dc973b065d3c2c6df4cf2a962769f6b1f9d
Angelo Verlain <geoangercola@gmail.com>
2024-08-12 18:50:45 +0200

    port: Boxed Lists

--- src/Boxed Lists/main.ts
+++ src/Boxed Lists/main.ts
@@ -1,14 +1,16 @@
+import Adw from "gi://Adw";
 import GObject from "gi://GObject";
 import Gtk from "gi://Gtk?version=4.0";
 
-const drop_down = workbench.builder.get_object("drop_down");
+const drop_down = workbench.builder.get_object<Adw.ComboRow>("drop_down");
 
 drop_down.connect("notify::selected-item", () => {
-  const selected_item = drop_down.selected_item.get_string();
-  console.log(selected_item);
+  const selected_item = drop_down.selected_item as Gtk.StringObject;
+  const selected_string = selected_item.get_string();
+  console.log(selected_string);
 });
 
-const expression = new Gtk.ClosureExpression(
+const expression = Gtk.ClosureExpression.new(
   GObject.TYPE_STRING,
   (obj) => obj.string,
   null,
