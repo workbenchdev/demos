0816d7b5d8732463955892ad591df877b2eba9f4
Angelo Verlain <geoangercola@gmail.com>
2024-08-20 20:05:58 +0200

    port: Dialog

--- src/Dialog/main.ts
+++ src/Dialog/main.ts
@@ -1,8 +1,10 @@
+import Adw from "gi://Adw";
 import Gio from "gi://Gio";
+import Gtk from "gi://Gtk?version=4.0";
 
-const dialog = workbench.builder.get_object("dialog");
-const button = workbench.builder.get_object("button");
-const image = workbench.builder.get_object("image");
+const dialog = workbench.builder.get_object<Adw.Dialog>("dialog");
+const button = workbench.builder.get_object<Gtk.Button>("button");
+const image = workbench.builder.get_object<Gtk.Image>("image");
 
 image.file = Gio.File.new_for_uri(workbench.resolve("image.svg")).get_path();
 
