db0556eb220b2ac7b2227c543ee8056da12c1630
Angelo Verlain <geoangercola@gmail.com>
2024-08-20 19:55:53 +0200

    port: Color Picker

--- src/Color Picker/main.ts
+++ src/Color Picker/main.ts
@@ -1,5 +1,6 @@
 import Gdk from "gi://Gdk?version=4.0";
 import Gio from "gi://Gio";
+import GLib from "gi://GLib";
 import Xdp from "gi://Xdp";
 import XdpGtk from "gi://XdpGtk4";
 
@@ -11,8 +12,9 @@ const button = workbench.builder.get_object("button");
 
 async function onClicked() {
   // result is a GVariant of the form (ddd), containing red, green and blue components in the range [0,1]
-  const result = await portal.pick_color(parent, null);
-  const [r, g, b] = result.deepUnpack();
+  // @ts-expect-error undetected async function
+  const result = await portal.pick_color(parent, null) as GLib.Variant<"(ddd)">;
+  const [r, g, b] = result.deepUnpack<number[]>();
 
   const color = new Gdk.RGBA({ red: r, green: g, blue: b, alpha: 1.0 });
   console.log(`Selected color is: ${color.to_string()}`);
