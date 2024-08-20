dc2e49c5cff33a0ddf946136d5931d943f7e859e
Angelo Verlain <geoangercola@gmail.com>
2024-08-20 19:45:48 +0200

    port: Camera

--- src/Camera/main.ts
+++ src/Camera/main.ts
@@ -1,7 +1,26 @@
+import Gdk from "gi://Gdk?version=4.0";
 import Gio from "gi://Gio";
+import Gsk from "gi://Gsk?version=4.0";
 import Gst from "gi://Gst";
+import GstVideo from "gi://GstVideo";
+import Gtk from "gi://Gtk?version=4.0";
 import Xdp from "gi://Xdp";
 import XdpGtk from "gi://XdpGtk4";
+
+interface GstGtk4Paintable extends Gdk.Paintable {
+  background_color: number;
+  force_aspect_ratio: number;
+  gl_context?: Gdk.GLContext;
+  orientation: number;
+  scaling_filter: Gsk.ScalingFilter;
+  use_scaling_filter: number;
+}
+
+interface GstGtk4PaintableSink extends GstVideo.VideoSink {
+  paintable: GstGtk4Paintable;
+  window_height: number;
+  window_width: number;
+}
 
 Gst.init(null);
 Gio._promisify(Xdp.Portal.prototype, "access_camera", "access_camera_finish");
@@ -9,7 +28,7 @@ Gio._promisify(Xdp.Portal.prototype, "access_camera", "access_camera_finish");
 const portal = new Xdp.Portal();
 const parent = XdpGtk.parent_new_gtk(workbench.window);
 
-const output = workbench.builder.get_object("output");
+const output = workbench.builder.get_object<Gtk.Picture>("output");
 const button = workbench.builder.get_object("button");
 
 button.connect("clicked", () => {
@@ -22,11 +41,12 @@ async function accessCamera() {
     return;
   }
 
+  // @ts-expect-error an undetected async function
   const success = await portal.access_camera(
     parent,
     Xdp.CameraFlags.NONE,
     null,
-  );
+  ) as boolean;
 
   if (!success) {
     console.log("Permission denied");
@@ -49,7 +69,7 @@ async function handleCamera() {
   const paintable_sink = Gst.ElementFactory.make(
     "gtk4paintablesink",
     "paintable_sink",
-  );
+  ) as GstGtk4PaintableSink;
   const glsinkbin = Gst.ElementFactory.make("glsinkbin", "glsinkbin");
 
   // Set up and Link Pipeline
@@ -62,8 +82,7 @@ async function handleCamera() {
   source.link(queue);
   queue.link(glsinkbin);
 
-  const paintable = paintable_sink.get_property("paintable");
-  output.paintable = paintable;
+  output.paintable = paintable_sink.paintable;
 
   // Start the pipeline
   pipeline.set_state(Gst.State.PLAYING);
