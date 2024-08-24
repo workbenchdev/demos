b6807483126d110aabd13d482b81ce57c29f43a9
Angelo Verlain <geoangercola@gmail.com>
2024-08-24 19:50:26 +0200

    port: View Switcher

--- src/View Switcher/main.ts
+++ src/View Switcher/main.ts
@@ -1,8 +1,12 @@
 import Adw from "gi://Adw";
 import Gtk from "gi://Gtk?version=4.0";
 
-const notifications_page = workbench.builder.get_object("page3");
-const notification_list = workbench.builder.get_object("notification_list");
+const notifications_page = workbench.builder.get_object<Adw.ViewStackPage>(
+  "page3",
+);
+const notification_list = workbench.builder.get_object<Gtk.ListBox>(
+  "notification_list",
+);
 
 const notification_count = 5;
 notifications_page.badge_number = notification_count;
@@ -14,10 +18,10 @@ for (let i = 0; i < notification_count; i++) {
   });
 
   const button = new Gtk.Button({
-    halign: "center",
-    valign: "center",
-    margin_top: "10",
-    margin_bottom: "10",
+    halign: Gtk.Align.CENTER,
+    valign: Gtk.Align.CENTER,
+    margin_top: 10,
+    margin_bottom: 10,
     icon_name: "check-plain-symbolic",
   });
 
