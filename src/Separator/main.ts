dc34fbafe381748fc5ed1de7c1dd46fa6ca1206c
Angelo Verlain <geoangercola@gmail.com>
2024-08-24 20:18:00 +0200

    port: Separator

--- src/Separator/main.ts
+++ src/Separator/main.ts
@@ -1,7 +1,8 @@
 import Gio from "gi://Gio";
+import Gtk from "gi://Gtk?version=4.0";
 
-const picture_one = workbench.builder.get_object("picture_one");
-const picture_two = workbench.builder.get_object("picture_two");
+const picture_one = workbench.builder.get_object<Gtk.Picture>("picture_one");
+const picture_two = workbench.builder.get_object<Gtk.Picture>("picture_two");
 
 const file = Gio.File.new_for_uri(workbench.resolve("./image.png"));
 
