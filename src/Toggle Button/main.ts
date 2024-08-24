1166fa3ee637bdcd4ff6acdfda97989493c6d047
Angelo Verlain <geoangercola@gmail.com>
2024-08-24 19:51:11 +0200

    port: Toggle Button

--- src/Toggle Button/main.ts
+++ src/Toggle Button/main.ts
@@ -1,3 +1,5 @@
+import Gtk from "gi://Gtk?version=4.0";
+
 const buttons = {
   button_no_look: "Don't look",
   button_look: "Look",
@@ -7,7 +9,7 @@ const buttons = {
 };
 
 for (const [id, name] of Object.entries(buttons)) {
-  const button = workbench.builder.get_object(id);
+  const button = workbench.builder.get_object<Gtk.ToggleButton>(id);
   button.connect("notify::active", () => {
     console.log(`${name} ${button.active ? "On" : "Off"}`);
   });
