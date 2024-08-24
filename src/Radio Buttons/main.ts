4bd3c0a9fcc74e4e3d7aa0af934574134d7a0b5b
Angelo Verlain <geoangercola@gmail.com>
2024-08-24 20:47:32 +0200

    port: Radio Buttons

--- src/Radio Buttons/main.ts
+++ src/Radio Buttons/main.ts
@@ -1,5 +1,11 @@
-const radio_button_1 = workbench.builder.get_object("radio_button_1");
-const radio_button_2 = workbench.builder.get_object("radio_button_2");
+import Gtk from "gi://Gtk?version=4.0";
+
+const radio_button_1 = workbench.builder.get_object<Gtk.ToggleButton>(
+  "radio_button_1",
+);
+const radio_button_2 = workbench.builder.get_object<Gtk.ToggleButton>(
+  "radio_button_2",
+);
 
 radio_button_1.connect("toggled", () => {
   if (radio_button_1.active) console.log("Force Light Mode");
