ac33cf9fbdafdeec3e49e67ed9d28a8d71943f1a
Angelo Verlain <geoangercola@gmail.com>
2024-09-03 18:25:18 +0200

    port: Menu Button

--- src/Menu Button/main.ts
+++ src/Menu Button/main.ts
@@ -1,5 +1,12 @@
-const circular_switch = workbench.builder.get_object("circular_switch");
-const secondary_button = workbench.builder.get_object("secondary");
+import Adw from "gi://Adw";
+import Gtk from "gi://Gtk?version=4.0";
+
+const circular_switch = workbench.builder.get_object<Adw.SwitchRow>(
+  "circular_switch",
+);
+const secondary_button = workbench.builder.get_object<Gtk.MenuButton>(
+  "secondary",
+);
 
 circular_switch.connect("notify::active", () => {
   if (circular_switch.active) {
