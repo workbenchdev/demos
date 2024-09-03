c1e75859b39bd3834a4c34cf34ec0d50ce7c8407
Angelo Verlain <geoangercola@gmail.com>
2024-09-03 17:54:51 +0200

    port: Overlay Split View

--- src/Overlay Split View/main.ts
+++ src/Overlay Split View/main.ts
@@ -1,6 +1,9 @@
+import Adw from "gi://Adw?version=1";
 import Gtk from "gi://Gtk?version=4.0";
 
-const split_view = workbench.builder.get_object("split_view");
+const split_view = workbench.builder.get_object<Adw.OverlaySplitView>(
+  "split_view",
+);
 const start_toggle = workbench.builder.get_object("start_toggle");
 const end_toggle = workbench.builder.get_object("end_toggle");
 
