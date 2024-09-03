3a963c7936c5d4a017587d26920de24b4264e875
Angelo Verlain <geoangercola@gmail.com>
2024-09-03 18:21:25 +0200

    port: Network Monitor

--- src/Network Monitor/main.ts
+++ src/Network Monitor/main.ts
@@ -1,8 +1,10 @@
+import Adw from "gi://Adw";
 import Gio from "gi://Gio";
+import Gtk from "gi://Gtk?version=4.0";
 
-const banner = workbench.builder.get_object("banner");
+const banner = workbench.builder.get_object<Adw.Banner>("banner");
 const network_monitor = Gio.NetworkMonitor.get_default();
-const level_bar = workbench.builder.get_object("level_bar");
+const level_bar = workbench.builder.get_object<Gtk.LevelBar>("level_bar");
 
 function setNetworkStatus() {
   banner.revealed = network_monitor.network_metered;
