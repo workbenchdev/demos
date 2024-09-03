ee0dda6af01bacba8cd3148665ed1e1415081eb0
Angelo Verlain <geoangercola@gmail.com>
2024-09-03 17:54:16 +0200

    port: Overlay

--- src/Overlay/main.ts
+++ src/Overlay/main.ts
@@ -1,6 +1,7 @@
 import Gio from "gi://Gio";
+import Gtk from "gi://Gtk?version=4.0";
 
 const file = Gio.File.new_for_uri(workbench.resolve("./image.png"));
 
-const picture = workbench.builder.get_object("picture");
+const picture = workbench.builder.get_object<Gtk.Picture>("picture");
 picture.file = file;
