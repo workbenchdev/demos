5dd3613fee3446985125678d5ff0dcc9abf99631
Angelo Verlain <geoangercola@gmail.com>
2024-09-03 17:53:38 +0200

    port: Picture

--- src/Picture/main.ts
+++ src/Picture/main.ts
@@ -1,9 +1,16 @@
 import Gio from "gi://Gio";
+import Gtk from "gi://Gtk?version=4.0";
 
-const picture_fill = workbench.builder.get_object("picture_fill");
-const picture_contain = workbench.builder.get_object("picture_contain");
-const picture_cover = workbench.builder.get_object("picture_cover");
-const picture_scale_down = workbench.builder.get_object("picture_scale_down");
+const picture_fill = workbench.builder.get_object<Gtk.Picture>("picture_fill");
+const picture_contain = workbench.builder.get_object<Gtk.Picture>(
+  "picture_contain",
+);
+const picture_cover = workbench.builder.get_object<Gtk.Picture>(
+  "picture_cover",
+);
+const picture_scale_down = workbench.builder.get_object<Gtk.Picture>(
+  "picture_scale_down",
+);
 
 const file = Gio.File.new_for_uri(workbench.resolve("./keys.png"));
 
