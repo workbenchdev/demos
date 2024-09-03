f38a0e7582f5d122d7641c522d39586b255143b5
Angelo Verlain <geoangercola@gmail.com>
2024-09-03 18:43:07 +0200

    port: Image

--- src/Image/main.ts
+++ src/Image/main.ts
@@ -1,9 +1,10 @@
 import Gio from "gi://Gio";
+import Gtk from "gi://Gtk?version=4.0";
 
 const path = Gio.File.new_for_uri(
   workbench.resolve("workbench.png"),
 ).get_path();
 
-workbench.builder.get_object("icon1").file = path;
-workbench.builder.get_object("icon2").file = path;
-workbench.builder.get_object("icon3").file = path;
+workbench.builder.get_object<Gtk.Image>("icon1").file = path;
+workbench.builder.get_object<Gtk.Image>("icon2").file = path;
+workbench.builder.get_object<Gtk.Image>("icon3").file = path;
