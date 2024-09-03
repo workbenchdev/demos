b3fcb00aa8d45012560c48e11addf09ce1c3071b
Angelo Verlain <geoangercola@gmail.com>
2024-09-03 18:23:03 +0200

    port: Navigation View

--- src/Navigation View/main.ts
+++ src/Navigation View/main.ts
@@ -1,11 +1,24 @@
-const nav_view = workbench.builder.get_object("nav_view");
-const nav_pageone = workbench.builder.get_object("nav_pageone");
-const next_button = workbench.builder.get_object("next_button");
-const previous_button = workbench.builder.get_object("previous_button");
-const nav_pagetwo = workbench.builder.get_object("nav_pagetwo");
-const nav_pagethree = workbench.builder.get_object("nav_pagethree");
-const nav_pagefour = workbench.builder.get_object("nav_pagefour");
-const title = workbench.builder.get_object("title");
+import Adw from "gi://Adw";
+import Gtk from "gi://Gtk?version=4.0";
+
+const nav_view = workbench.builder.get_object<Adw.NavigationView>("nav_view");
+const nav_pageone = workbench.builder.get_object<Adw.NavigationPage>(
+  "nav_pageone",
+);
+const next_button = workbench.builder.get_object<Gtk.Button>("next_button");
+const previous_button = workbench.builder.get_object<Gtk.Button>(
+  "previous_button",
+);
+const nav_pagetwo = workbench.builder.get_object<Adw.NavigationPage>(
+  "nav_pagetwo",
+);
+const nav_pagethree = workbench.builder.get_object<Adw.NavigationPage>(
+  "nav_pagethree",
+);
+const nav_pagefour = workbench.builder.get_object<Adw.NavigationPage>(
+  "nav_pagefour",
+);
+const title = workbench.builder.get_object<Gtk.Label>("title");
 
 next_button.connect("clicked", () => {
   switch (nav_view.visible_page) {
