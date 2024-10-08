e1f33bb4831eccfd7c39b28f47d0fc847bca37f2
Nokse22 <44558032+Nokse22@users.noreply.github.com>
2024-10-08 10:55:32 +0200

    Fixed Grid View python port CI

--- src/Grid View/main.py
+++ src/Grid View/main.py
@@ -2,7 +2,7 @@ import gi
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
-from gi.repository import Gtk, Adw
+from gi.repository import Gtk
 import workbench
 
 grid_view = workbench.builder.get_object("grid_view")
