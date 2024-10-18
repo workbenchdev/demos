69f607921195d78751e120701cce561f1e33a2ea
Sonny Piers <sonny@fastmail.net>
2024-10-18 13:02:46 +0200

    tweaks

--- src/Database/main.js
+++ src/Database/main.js
@@ -1,13 +1,32 @@
 import GObject from "gi://GObject";
 import Gom from "gi://Gom";
 import Gtk from "gi://Gtk";
-import Adw from "gi://Adw";
 import GLib from "gi://GLib";
 import Gio from "gi://Gio";
 
+Gio._promisify(Gom.Adapter.prototype, "open_async", "open_finish");
+
+Gio._promisify(
+  Gom.Repository.prototype,
+  "automatic_migrate_async",
+  "automatic_migrate_finish",
+);
+Gio._promisify(Gom.Repository.prototype, "find_async", "find_finish");
+Gio._promisify(Gom.Repository.prototype, "find_one_async", "find_one_finish");
+
+Gio._promisify(Gom.Resource.prototype, "save_async", "save_finish");
+Gio._promisify(Gom.ResourceGroup.prototype, "fetch_async", "fetch_finish");
+
+const text_entry = workbench.builder.get_object("text_entry");
+const id_entry = workbench.builder.get_object("id_entry");
+const insert_button = workbench.builder.get_object("insert_button");
+const search_entry = workbench.builder.get_object("search_entry");
+const column_view = workbench.builder.get_object("column_view");
+const column_text = workbench.builder.get_object("column_text");
+const column_id = workbench.builder.get_object("column_id");
+
 const ItemClass = GObject.registerClass(
   {
-    GTypeName: "Item",
     Properties: {
       id: GObject.ParamSpec.int(
         "id",
@@ -18,10 +37,10 @@ const ItemClass = GObject.registerClass(
         GLib.MAXINT32,
         0,
       ),
-      url: GObject.ParamSpec.string(
-        "url",
-        "URL",
-        "A URL",
+      text: GObject.ParamSpec.string(
+        "text",
+        "Text",
+        "Some Text",
         GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT,
         "",
       ),
@@ -30,90 +49,82 @@ const ItemClass = GObject.registerClass(
   class ItemClass extends Gom.Resource {},
 );
 
-let adapter, repository;
+const data_model = new Gio.ListStore({ item_type: ItemClass });
+let adapter;
+let repository;
 
-function initDatabase() {
+async function initDatabase() {
   adapter = new Gom.Adapter();
-  adapter.open_sync(workbench.resolve("/gom-js-test.db"));
-  repository = new Gom.Repository({ adapter: adapter });
+  await adapter.open_async(workbench.resolve("db.sqlite"));
+  repository = new Gom.Repository({ adapter });
 
   // Set up table and primary key
   ItemClass.set_table("items");
   ItemClass.set_primary_key("id");
 
   // Perform automatic migration
-  repository.automatic_migrate_sync(1, [ItemClass]);
+  await repository.automatic_migrate_async(1, [ItemClass]);
+
+  await search();
 }
 
-function closeDatabase() {
-  if (adapter) {
-    adapter.close_sync();
-  }
-}
+initDatabase().catch(console.error);
 
-initDatabase();
-
-const text_entry = workbench.builder.get_object("text_entry");
-const id_entry = workbench.builder.get_object("id_entry");
-const insert_button = workbench.builder.get_object("insert_button");
-const search_entry = workbench.builder.get_object("search_entry");
-const result_label = workbench.builder.get_object("result_label");
-const data_model = new Gio.ListStore({ item_type: ItemClass });
-const column_view = workbench.builder.get_object("column_view");
-const col1 = workbench.builder.get_object("col1");
-const col2 = workbench.builder.get_object("col2");
-let count = 0;
-
-insert_button.connect("clicked", () => {
-  const url = text_entry.text;
-  const item = new ItemClass({ repository: repository, url: url });
-  const success = item.save_sync();
-
-  if (success) {
-    data_model.append(item);
-    id_entry.set_range(1, ++count);
-  }
-});
-
-id_entry.connect("value-changed", () => {
-  data_model.remove_all();
-  const id = parseInt(id_entry.text);
-  const filter = Gom.Filter.new_eq(ItemClass, "id", id);
-  const found_item = repository.find_one_sync(ItemClass, filter);
-  if (found_item) {
-    data_model.append(found_item);
-  } else {
-    result_label.label = "Item not found";
-  }
-});
-
-search_entry.connect("search-changed", () => {
-  data_model.remove_all();
-  const filter_text = search_entry.text.trim();
-  if (filter_text === "") {
-    result_label.label = "";
+async function onInsert() {
+  const text = text_entry.text;
+  const item = new ItemClass({ repository, text });
+  const success = await item.save_async();
+  if (!success) {
+    console.error("Failed to insert");
     return;
   }
-  // Create a filter for Text matching
-  const filter = Gom.Filter.new_glob(ItemClass, "url", `*${filter_text}*`);
-  const filtered_items = repository.find_sync(ItemClass, filter);
 
-  if (filtered_items && filtered_items.get_count() > 0) {
-    filtered_items.fetch_async(0, filtered_items.get_count(), () => {
-      for (let i = 0; i < filtered_items.get_count(); i++) {
-        const item = filtered_items.get_index(i);
-        if (item) data_model.append(item);
-      }
-      result_label.label = "Loaded successfully";
-    });
-  } else {
-    result_label.label = "No matching items found";
-  }
+  data_model.append(item);
+}
+
+insert_button.connect("clicked", () => {
+  onInsert().catch(console.error);
 });
 
-const factory_col1 = col1.factory;
+async function findById() {
+  const { text } = id_entry;
+  if (!text) {
+    return;
+  }
 
-factory_col1.connect("setup", (_self, list_item) => {
+  data_model.remove_all();
+  const id = parseInt(text);
+  const filter = Gom.Filter.new_eq(ItemClass, "id", id);
+  const found_item = await repository.find_one_async(ItemClass, filter);
+  if (found_item) {
+    data_model.append(found_item);
+  }
+}
+
+id_entry.connect("search-changed", () => {
+  findById().catch(console.error);
+});
+
+async function search() {
+  data_model.remove_all();
+  const filter_text = search_entry.text.trim();
+
+  // Create a filter for Text matching
+  const filter = Gom.Filter.new_glob(ItemClass, "text", `*${filter_text}*`);
+  const resource_group = await repository.find_async(ItemClass, filter);
+
+  await resource_group.fetch_async(0, resource_group.count);
+  for (let i = 0; i < resource_group.count; i++) {
+    const item = resource_group.get_index(i);
+    if (item) data_model.append(item);
+  }
+}
+
+search_entry.connect("search-changed", () => {
+  search().catch(console.error);
+});
+
+column_text.factory.connect("setup", (_self, list_item) => {
   const label = new Gtk.Label({
     margin_start: 12,
     margin_end: 12,
@@ -121,14 +132,13 @@ factory_col1.connect("setup", (_self, list_item) => {
   list_item.set_child(label);
 });
 
-factory_col1.connect("bind", (_self, list_item) => {
+column_text.factory.connect("bind", (_self, list_item) => {
   const label_widget = list_item.get_child();
   const model_item = list_item.get_item();
-  label_widget.label = model_item.url;
+  label_widget.label = model_item.text;
 });
 
-const factory_col2 = col2.factory;
-factory_col2.connect("setup", (_self, list_item) => {
+column_id.factory.connect("setup", (_self, list_item) => {
   const label = new Gtk.Label({
     margin_start: 12,
     margin_end: 12,
@@ -136,7 +146,7 @@ factory_col2.connect("setup", (_self, list_item) => {
   list_item.set_child(label);
 });
 
-factory_col2.connect("bind", (_self, list_item) => {
+column_id.factory.connect("bind", (_self, list_item) => {
   const label_widget = list_item.get_child();
   const model_item = list_item.get_item();
   label_widget.label = model_item.id.toString();
