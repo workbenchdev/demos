41fc6c5c0800205ec718c588b2921b17c2e30583
Angelo Verlain <geoangercola@gmail.com>
2024-10-31 14:09:06 +0200

    port: Gamepad

--- src/Gamepad/main.ts
+++ src/Gamepad/main.ts
@@ -1,9 +1,10 @@
+import Gtk from "gi://Gtk?version=4.0";
 import Manette from "gi://Manette";
 
-const stack = workbench.builder.get_object("stack");
+const stack = workbench.builder.get_object<Gtk.Stack>("stack");
 const button_rumble = workbench.builder.get_object("button_rumble");
 
-const devices = new Set();
+const devices = new Set<Manette.Device>();
 
 stack.visible_child_name = "connect";
 button_rumble.connect("clicked", () => {
@@ -14,14 +15,16 @@ button_rumble.connect("clicked", () => {
   }
 });
 
-function onDevice(device) {
+function onDevice(device: Manette.Device) {
   console.log("Device connected:", device.get_name());
 
   // Face and Shoulder Buttons
   device.connect("button-press-event", (device, event) => {
     const [success, button] = event.get_button();
     console.log(
-      `${device.get_name()}: press ${success ? button : event.get_hardware_code()}`,
+      `${device.get_name()}: press ${
+        success ? button : event.get_hardware_code()
+      }`,
     );
   });
 
@@ -29,7 +32,9 @@ function onDevice(device) {
   device.connect("button-release-event", (device, event) => {
     const [success, button] = event.get_button();
     console.log(
-      `${device.get_name()}: release ${success ? button : event.get_hardware_code()}`,
+      `${device.get_name()}: release ${
+        success ? button : event.get_hardware_code()
+      }`,
     );
   });
 
@@ -42,8 +47,9 @@ function onDevice(device) {
   // Analog Axis - Triggers and Joysticks
   device.connect("absolute-axis-event", (device, event) => {
     const [, axis, value] = event.get_absolute();
-    if (Math.abs(value) > 0.2)
+    if (Math.abs(value) > 0.2) {
       console.log(`${device.get_name()}: moved axis ${axis} to ${value}`);
+    }
   });
 
   devices.add(device);
@@ -60,14 +66,14 @@ function onDeviceDisconnected(device) {
 const monitor = new Manette.Monitor();
 const monitor_iter = monitor.iterate();
 
-let has_next;
-let device;
+let has_next: boolean, device: Manette.Device | null;
 do {
   [has_next, device] = monitor_iter.next();
   if (device !== null) onDevice(device);
 } while (has_next);
 
 monitor.connect("device-connected", (_self, device) => onDevice(device));
-monitor.connect("device-disconnected", (_self, device) =>
-  onDeviceDisconnected(device),
+monitor.connect(
+  "device-disconnected",
+  (_self, device) => onDeviceDisconnected(device),
 );
