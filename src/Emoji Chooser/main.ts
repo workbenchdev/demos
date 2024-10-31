f97932765f284e2056bd5b8399a1e79717850314
Urtsi Santsi <urtsi.santsi@proton.me>
2024-11-01 00:21:35 +0200

    Use generic get_object

--- src/Emoji Chooser/main.ts
+++ src/Emoji Chooser/main.ts
@@ -1,6 +1,6 @@
 import Gtk from "gi://Gtk?version=4.0";
 
-const emoji_chooser = workbench.builder.get_object("emoji_chooser");
+const emoji_chooser = workbench.builder.get_object<Gtk.EmojiChooser>("emoji_chooser");
 const button = workbench.builder.get_object<Gtk.Button>("button");
 
 emoji_chooser.connect("emoji-picked", (_self, emoji) => {
