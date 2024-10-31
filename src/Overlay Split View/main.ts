59ae29b4d5a004125a22c04a95a95be130697054
Urtsi Santsi <urtsi.santsi@proton.me>
2024-11-01 00:36:00 +0200

    Use generic get_object

--- src/Overlay Split View/main.ts
+++ src/Overlay Split View/main.ts
@@ -4,8 +4,8 @@ import Gtk from "gi://Gtk?version=4.0";
 const split_view = workbench.builder.get_object<Adw.OverlaySplitView>(
   "split_view",
 );
-const start_toggle = workbench.builder.get_object("start_toggle");
-const end_toggle = workbench.builder.get_object("end_toggle");
+const start_toggle = workbench.builder.get_object<Gtk.ToggleButton>("start_toggle");
+const end_toggle = workbench.builder.get_object<Gtk.ToggleButton>("end_toggle");
 
 start_toggle.connect("toggled", () => {
   split_view.sidebar_position = Gtk.PackType.START;
