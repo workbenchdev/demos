5a8c6b0e46900852e00f633096bd1745413d477d
Urtsi Santsi <urtsi.santsi@proton.me>
2024-10-31 20:39:35 +0200

    Use generic get_object

--- src/Web View/main.ts
+++ src/Web View/main.ts
@@ -3,10 +3,10 @@ import GObject from "gi://GObject";
 import Gtk from "gi://Gtk?version=4.0";
 import WebKit from "gi://WebKit";
 
-const button_back = workbench.builder.get_object("button_back");
-const button_forward = workbench.builder.get_object("button_forward");
-const button_reload = workbench.builder.get_object("button_reload");
-const button_stop = workbench.builder.get_object("button_stop");
+const button_back = workbench.builder.get_object<Gtk.Button>("button_back");
+const button_forward = workbench.builder.get_object<Gtk.Button>("button_forward");
+const button_reload = workbench.builder.get_object<Gtk.Button>("button_reload");
+const button_stop = workbench.builder.get_object<Gtk.Button>("button_stop");
 const url_bar = workbench.builder.get_object<Gtk.Entry>("url_bar");
 const web_view = workbench.builder.get_object<WebKit.WebView>("web_view");
 
