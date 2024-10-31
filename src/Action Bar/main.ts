e1b164d5e583b58d53041bb3fff6afb48cd10403
Urtsi Santsi <urtsi.santsi@proton.me>
2024-10-31 20:42:51 +0200

    Use generic get_object

--- src/Action Bar/main.ts
+++ src/Action Bar/main.ts
@@ -2,8 +2,8 @@ import Gtk from "gi://Gtk?version=4.0";
 
 const action_bar = workbench.builder.get_object<Gtk.ActionBar>("action_bar");
 const button = workbench.builder.get_object<Gtk.ToggleButton>("button");
-const start_widget = workbench.builder.get_object("start_widget");
-const end_widget = workbench.builder.get_object("end_widget");
+const start_widget = workbench.builder.get_object<Gtk.Button>("start_widget");
+const end_widget = workbench.builder.get_object<Gtk.Button>("end_widget");
 
 button.connect("notify::active", () => {
   action_bar.revealed = !button.active;
