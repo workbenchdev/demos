dc0f4e009f92128ff3e549f8927d6743669882c1
Urtsi Santsi <urtsi.santsi@proton.me>
2024-11-01 00:39:02 +0200

    Use generic get_object

--- src/Preferences Dialog/main.ts
+++ src/Preferences Dialog/main.ts
@@ -1,13 +1,14 @@
+import Gtk from "gi://Gtk?version=4.0";
 import Adw from "gi://Adw";
 
 const dialog = workbench.builder.get_object<Adw.PreferencesDialog>("dialog");
 const dm_switch = workbench.builder.get_object<Adw.SwitchRow>("dm_switch");
 const subpage = workbench.builder.get_object<Adw.NavigationPage>("subpage");
-const subpage_row = workbench.builder.get_object("subpage_row");
-const subpage_button = workbench.builder.get_object("subpage_button");
-const toast_button = workbench.builder.get_object("toast_button");
+const subpage_row = workbench.builder.get_object<Adw.ActionRow>("subpage_row");
+const subpage_button = workbench.builder.get_object<Gtk.Button>("subpage_button");
+const toast_button = workbench.builder.get_object<Gtk.Button>("toast_button");
 const style_manager = Adw.StyleManager.get_default();
-const button = workbench.builder.get_object("button");
+const button = workbench.builder.get_object<Gtk.Button>("button");
 
 dm_switch.active = style_manager.dark;
 
